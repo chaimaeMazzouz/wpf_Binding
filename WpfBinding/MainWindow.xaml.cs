efdaeedd91ba5ee5e07012edfef488b4d49219b6
chaimaeMazzouz <mazzouz.chaimae.dev@gmail.com>
2021-03-02 21:12:04 +0100

    not done

--- WpfBinding/MainWindow.xaml.cs
+++ WpfBinding/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfBinding
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
